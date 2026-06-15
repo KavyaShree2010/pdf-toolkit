70185b782ee1c0eb46302e57ec57862f133b9e98
Kavya Shree S <ks6705@srmist.edu.in>
2026-06-15 11:12:07 +0530

    Refactor PDF merging functionality and add PDF info retrieval

--- pdf_merger.py
+++ pdf_merger.py
@@ -1,15 +1,83 @@
-from pypdf import PdfWriter
+from pypdf import PdfWriter, PdfReader
+import os
 
-pdf1 = input("Enter first pdf name: ")
-pdf2 = input("Enter second pdf name: ")
+def merge_pdfs():
+    files = []
 
-merger = PdfWriter()
+    while True:
+        name = input("Enter the pdf filename (or 'done' to finish): ")
+        if name.lower() == 'done':
+            break
+        if not os.path.isfile(name):
+            print(f"Error: {name} not found. Try again.")
+            continue
+        if not name.endswith('.pdf'):
+            print(f"Error: {name} is not a PDF file.")
+            continue
 
-merger.append(pdf1)
-merger.append(pdf2)
+        files.append(name)
+    if not files:
+        print("No PDF files to merge. Exiting.")
+        return
 
-merger.write("merged_pdf.pdf")
+    writer = PdfWriter()
 
-merger.close()
+    try:
+     for f in files:
+        writer.append(f)
 
-print("PDF files merged successfully into 'merged_pdf.pdf'.")
+     output = input("Enter output filename: ")
+     if not output.ednswith('.pdf'):
+         output += '.pdf'
+
+
+     writer.write(output)
+     writer.close()
+
+     print(f"PDF files merged successfully into {output}")
+
+    except Exception as e:
+        print(f"Something went wrong: {e}")
+
+def pdf_info():
+    filename = input("Enter pdf filename: ")
+    if not os.path.isfile(filename):
+        print(f"Error: {filename} not found.")
+        return
+    try:
+     reader = PdfReader(filename)
+     size = os.path.getsize(filename)
+     meta = reader.metadata
+
+     print(f"\n--PDF INFO--")
+     print(f"Pages     : {len(reader.pages)}")
+     print(f"Size      :{round(size/1024, 2)}KB")
+     print(f"Title     :{meta.title if meta.title else 'Not Available'}")
+     print(f"Author     :{meta.author if meta.author else 'Not Available'}")
+     print(f"Creator   :{meta.creator if meta.creator else 'Not Available'}")
+
+    except Exception as e:
+        print(f"Something went wrong: {e}")
+
+
+def main():
+    while True:
+        print("\n1. Merge PDFs")
+        print("\n2. Get PDF Info")
+        print("\n3. Exit")
+
+        choice = input(f"\nChoose an option: ").strip()
+        if choice == '1':
+           merge_pdfs()
+        elif choice == '2':
+            pdf_info()
+        elif choice == '3':
+            print("Bye.")
+            break  
+        else:
+            print("Invalid choice. Enter 1, 2 or 3.") 
+
+if __name__ == "__main__":
+    main()
+    
+
